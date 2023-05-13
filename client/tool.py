9c1b6d96f930b5cc5b11ca12f720ca2f16c7ec4e
ffffeng <2510465956@qq.com>
2023-05-13 19:37:22 +0800

    modified:   client/tool.py

--- client/tool.py
+++ client/tool.py
@@ -7,6 +7,5 @@ import cv2
 def bytes2cv(im):
     return cv2.imdecode(np.array(bytearray(im), dtype='uint8'), cv2.IMREAD_UNCHANGED)  # 从二进制图片数据中读取
 
-
 def cv2bytes(im):
     return np.array(cv2.imencode('.png', im)[1]).tobytes()
